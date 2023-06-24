059f6a9ee883577e9762cfb143cdcb757af9e6e5
Alex Fok <foktmalexander@gmail.com>
2023-06-24 07:02:20 -0700

    Add min-width for all sections

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,7 +1,8 @@
 import styles from './Section.module.css'
+
 function Section(props) {
     return (
-        <div class={styles.section} style={props.grow ? {'flex-grow': 1} : {}}>
+        <div class={styles.section}>
             <h4 class={styles.title}>{props.title}</h4>
             {props.children}
         </div>
