311d0027d7d6d5a0abb676fde39f866139cd3336
Alex Fok <foktmalexander@gmail.com>
2023-09-25 00:56:01 -0700

    Change hidden text 'GitHub' to always be visible

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,7 @@
-import { createSignal } from 'solid-js';
 import styles from './Header.module.css';
 import GitIcon from './icons/gitIcon';
 
 function Header() {
-
-    const [hidden, setHidden] = createSignal(true)
-
     return (
         <div class={styles.header}>
             <div class={styles.title}>alex-fok/homepage</div>
@@ -13,11 +9,8 @@ function Header() {
                 class={styles.icon}
                 href='https://github.com/alex-fok'
                 target='_blank'
-                onMouseEnter={() => { setHidden(false) }}
-                onMouseLeave={() => { setHidden(true) }}
-                
             >
-                <span class={hidden() ? styles.hidden : ''}>GitHub</span>
+                <span style='text-decoration: underline'>GitHub</span>
                 <GitIcon />
             </a>
         </div>
