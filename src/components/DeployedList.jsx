8c6385eee22b4cfde310adbee4a1b3a29b3ddaf4
Alex Fok <foktmalexander@gmail.com>
2023-09-25 00:54:39 -0700

    Add visual-physics to deployed apps. Add app screenshot

--- src/components/DeployedList.jsx
+++ src/components/DeployedList.jsx
@@ -2,23 +2,41 @@ import styles from './DeployedList.module.css'
 
 const apps = [
     {
-        name: 'minesweeper',
-        content: 'This is a 1 vs 1 turn-based minesweeper introduced more than a decade ago by MSN Messenger. The objective of the game is to reveal more mines than your opponent. Player who reveals more than half of the mines first wins the game.'
+        name: 'Minesweeper',
+        href: '/minesweeper',
+        content: 'A multiplayer turn-based minesweeper introduced more than ' + 
+        'a decade ago by MSN Messenger. The objective of the game is to reveal ' +
+        'more mines than your opponent. Player who reveals more than half of the mines first wins the game.',
+        thumbnail: 'minesweeper.png'
+    },
+    {
+        name: 'Visual Physics',
+        href: '/visual-physics',
+        content: 'A visualization of common physics equations.',
+        thumbnail: 'visual-physics.png'
     }
 ]
 
 function DeployedList() {
     return (
         <div class={styles.deployedList}>
-            <For each={apps}>{(app, i) =>
-                <div class={styles.app}>
-                    <a
+            <For each={apps}>{ app =>
+                <a
+                    class={styles.app}
+                    href={app.href}
+                >
+                    <div
                         class={styles.title}
-                        href={'/minesweeper'}
                         target='_blank'
-                    >{'>> '}{app.name}</a>
+                    >{'>> '}{app.name}</div>
+                    <div class={styles.imgWrapper}>
+                        <img
+                            src={'/images/' + app.thumbnail}
+                            style='width: 100%; height: auto; max-width: 20rem;'
+                        ></img>
+                    </div>
                     <div>{app.content}</div>
-                </div>
+                </a>
             }</For>
         </div>
     )
